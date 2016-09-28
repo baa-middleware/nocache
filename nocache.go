f1e2872f21d35feb1f3bb762af985d75107e29f4
coldstar <safeie@163.com>
2016-09-28 11:26:17 +0800

    implement functions

---
+++ nocache.go
@@ -0,0 +1,40 @@
+// Package nocache provider a baa middleware for http no-cache control.
+package nocache
+
+import (
+	"time"
+
+	baa "gopkg.in/baa.v1"
+)
+
+// Unix epoch time
+var epoch = time.Unix(0, 0).Format(time.RFC1123)
+
+// Taken from https://github.com/mytrile/nocache
+var noCacheHeaders = map[string]string{
+	"Expires":         epoch,
+	"Cache-Control":   "no-cache, private, max-age=0",
+	"Pragma":          "no-cache",
+	"X-Accel-Expires": "0",
+}
+
+var etagHeaders = []string{
+	"ETag",
+	"If-Modified-Since",
+	"If-Match",
+	"If-None-Match",
+	"If-Range",
+	"If-Unmodified-Since",
+}
+
+// New returns a baa middleware for http no-cache control
+func New() baa.HandlerFunc {
+	return func(c *baa.Context) {
+		// Set our NoCache headers
+		for k, v := range noCacheHeaders {
+			c.Resp.Header().Set(k, v)
+		}
+
+		c.Next()
+	}
+}
